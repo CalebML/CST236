8718b06bfd523989ef15443f861b07b4536fdeb5
CalebML <CalebML@users.noreply.github.com>
2015-05-18 15:59:07 -0700

    Added test more test for SharpTona

--- tests/ui_test.py
+++ tests/ui_test.py
@@ -22,8 +22,85 @@ class testUI(TestCase):
         if( self.app.sharpTona['Answer'].Texts()[0] != 'Answer: ' ):
             error = 'There is no Answer label'
             print self.app.sharpTona['Answer'].Texts()[0]
+        if( self.app.sharpTona['Teach'].IsEnabled() ):
+            error = 'Teach is enabled'
+        if( self.app.sharpTona['Correct'].IsEnabled() ):
+            error = 'Correct is enabled'
+        if( self.app.sharpTona['Answer:Edit'].IsEnabled() ):
+            error = 'Answer box is enabled'
         
         if( error ):
             print error
         self.assertTrue(error == None)
 
+    def test_ask_question(self):
+        error = None
+        self.app.sharpTona['Question:Edit'].TypeKeys('What is the answer to everything?', with_spaces=True)
+        self.app.sharpTona['Ask'].Click()
+
+        if( self.app.sharpTona['Answer:Edit'].Texts()[0] != '42' ):
+            error = 'Answer is not 42'
+        if( not self.app.sharpTona['Answer:Edit'].IsEnabled() ):
+            error = 'Answer box is not enabled after asking valid question'
+            
+        self.app.sharpTona['Answer:Edit'].TypeKeys('42.0', with_spaces=True)
+        self.app.sharpTona['Correct'].Click()
+
+        if( self.app.sharpTona['Teach'].IsEnabled() ):
+            error = 'Teach is enabled'
+        if( self.app.sharpTona['Correct'].IsEnabled() ):
+            error = 'Correct is enabled'
+        if( self.app.sharpTona['Answer:Edit'].IsEnabled() ):
+            error = 'Answer box is enabled'
+
+        self.app.sharpTona['Ask'].Click()
+
+        if( self.app.sharpTona['Answer:Edit'].Texts()[0] != '42.0' ):
+            error = 'Answer is not 42.0'
+            
+        if( error ):
+            print error
+        self.assertTrue(error == None)
+
+    def test_no_question(self):
+        error = None
+        self.app.sharpTona['Ask'].Click()
+
+        if( self.app.sharpTona['Answer:Edit'].Texts()[0] != 'Was that a question?' ):
+            error = 'Seeing a quesrion when there is none'
+
+        if( error ):
+            print error
+        self.assertTrue(error == None)
+
+    def test_Unknown_question(self):
+        error = None
+        self.app.sharpTona['Question:Edit'].TypeKeys('Who is Iron Man?', with_spaces=True)
+        self.app.sharpTona['Ask'].Click()
+
+        if( self.app.sharpTona['Answer:Edit'].Texts()[0] != 'I don\'t know please teach me.' ):
+            error = 'System reconizing unknown questions'
+        
+        if( not self.app.sharpTona['Teach'].IsEnabled() ):
+            error = 'Teach is not enabled'
+
+        self.app.sharpTona['Answer:Edit'].TypeKeys('Tony Stark', with_spaces=True)
+        self.app.sharpTona['Teach'].Click()
+
+        if( self.app.sharpTona['Teach'].IsEnabled() ):
+            error = 'Teach is enabled'
+        if( self.app.sharpTona['Correct'].IsEnabled() ):
+            error = 'Correct is enabled'
+        if( self.app.sharpTona['Answer:Edit'].IsEnabled() ):
+            error = 'Answer box is enabled'
+
+        self.app.sharpTona['Ask'].Click()
+
+        if( self.app.sharpTona['Answer:Edit'].Texts()[0] != 'Tony Stark' ):
+            error = 'System not saving taught answers'
+
+        if( error ):
+            print error
+        self.assertTrue(error == None)
+
+        
